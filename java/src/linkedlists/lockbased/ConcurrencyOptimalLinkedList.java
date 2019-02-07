104ac12113d67f4a37d02bb077d4d979ffa641f5
vaksenov <github12345>
2019-02-07 14:05:43 +0100

    Concurrency optimal linked list

---
+++ java/src/linkedlists/lockbased/ConcurrencyOptimalLinkedList.java
@@ -0,0 +1,185 @@
+package linkedlists.lockbased;
+
+import contention.abstractions.AbstractCompositionalIntSet;
+
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * Created by vaksenov on 07.02.2019.
+ */
+public class ConcurrencyOptimalLinkedList extends AbstractCompositionalIntSet {
+    public class Node {
+        int value;
+        volatile Node next;
+        volatile boolean deleted;
+
+        ReentrantLock lock;
+
+        public Node(int value) {
+            this.value = value;
+            lock = new ReentrantLock();
+        }
+
+        public boolean lock(Node next) {
+            if (this.next != next || deleted) {
+                return false;
+            }
+            lock.lock();
+            if (this.next != next || deleted) {
+                lock.unlock();
+                return false;
+            }
+            return true;
+        }
+
+        public boolean lock(int next) {
+            if (this.next.value != next || deleted) {
+                return false;
+            }
+            lock.lock();
+            if (this.next.value != next || deleted) {
+                lock.unlock();
+                return false;
+            }
+            return true;
+        }
+
+        public void unlock() {
+            lock.unlock();
+        }
+    }
+
+    Node head;
+
+    public ConcurrencyOptimalLinkedList() {
+        head = new Node(Integer.MIN_VALUE);
+        head.next = new Node(Integer.MAX_VALUE);
+    }
+
+    public class Window {
+        Node prev;
+        Node curr;
+
+        public Window() {
+            prev = head;
+        }
+
+        public void setValues(Node prev, Node curr) {
+            this.prev = prev;
+            this.curr = curr;
+        }
+    }
+
+    public Window traverse(final int value, Window window) {
+        Node prev = window.prev;
+        if (prev.deleted) {
+            prev = head;
+        }
+        Node curr = prev.next;
+        while (curr.value < value) {
+            prev = curr;
+            curr = curr.next;
+        }
+
+        window.setValues(prev, curr);
+        return window;
+    }
+
+    @Override
+    public boolean addInt(int x) {
+        Window window = new Window();
+
+        Node newNode = null;
+
+        Node prev, curr;
+
+        while (true) {
+            traverse(x, window);
+            curr = window.curr;
+            if (curr.deleted) {
+                continue;
+            }
+            if (curr.value == x) {
+                return false;
+            }
+
+            if (newNode == null) {
+                newNode = new Node(x);
+            }
+            newNode.next = curr;
+            prev = window.prev;
+            if (!prev.lock(curr)) {
+                continue;
+            }
+
+            prev.next = newNode;
+
+            prev.unlock();
+            return true;
+        }
+    }
+
+    @Override
+    public boolean removeInt(int x) {
+        Window window = new Window();
+
+        Node prev, curr;
+
+        while (true) {
+            traverse(x, window);
+            curr = window.curr;
+            if (curr.deleted) {
+                continue;
+            }
+            if (curr.value != x) {
+                return false;
+            }
+
+            Node next = curr.next;
+
+            prev = window.prev;
+            if (!prev.lock(x)) {
+                continue;
+            }
+
+            curr = prev.next;
+            if (!curr.lock(next)) {
+                prev.unlock();
+                continue;
+            }
+
+            curr.deleted = true;
+            prev.next = curr.next;
+
+            curr.unlock();
+            prev.unlock();
+            return true;
+        }
+    }
+
+    @Override
+    public boolean containsInt(int x) {
+        Node curr = head;
+        while (curr.value < x) {
+            curr = curr.next;
+        }
+        return curr.value == x && !curr.deleted;
+    }
+
+    @Override
+    public int size() {
+        int total = 0;
+        Node curr = head;
+        while (curr != null) {
+            total++;
+            curr = curr.next;
+        }
+        return total - 2;
+    }
+
+    @Override
+    public void clear() {
+        head = new Node(Integer.MIN_VALUE);
+        head.next = new Node(Integer.MAX_VALUE);
+    }
+}
